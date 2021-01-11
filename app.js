364f02818e0cd395a5be58cc2b095f81e45e77c9
Steveplays28 <62797992+Steveplays28@users.noreply.github.com>
2021-01-11 20:08:23 +0100

    Cleaned up the folder

--- app.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const Discord = require('discord.js') //this says that its using discord.js and classifing it as a bot
-const bot = new Discord.Client();
-
-const token = 'put your bots token here!';
-//link to the dev portal to make your own discord bot here: https://discord.com/developers/applications
-
-console.log('Hello world');
